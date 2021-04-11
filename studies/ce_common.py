3dccf566383e9263f40bcac7166ab4ff357f379c
ethanmac <ethanmac@mac.com>
2021-04-11 17:30:08 +0800

    update

--- studies/ce_common.py
+++ studies/ce_common.py
@@ -1,4 +1,26 @@
 import inspect
+import collections
+
+class HyperParameters(collections.OrderedDict):
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def get_exist_val(self, name, default_val):
+        if name in self:
+            return self[name]
+        return default_val
 
 class CeInspect(object):
     @classmethod
